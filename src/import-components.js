8c41f379aa3233f41adc9a8a9f495e56790948bb
Kurren Nischal <Kurren.Nischal@codis.co.uk>
2018-10-19 14:03:04 +0100

    minimal example

--- src/import-components.js
+++ src/import-components.js
@@ -1,3 +1,3 @@
-import '@webcomponents/webcomponentsjs/webcomponents-loader.js' //polyfill
 import '@polymer/polymer/lib/elements/dom-if.js';
 import '@polymer/paper-checkbox/paper-checkbox.js';
+import '@vaadin/vaadin-date-picker/vaadin-date-picker.js';
